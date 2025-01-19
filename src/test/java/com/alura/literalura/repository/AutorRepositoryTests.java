acf6ca005c29437dc89c15a42802c69056ea8f57
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2025-01-19 14:21:29 -0300

    test: adicionar testes para AutorRepository e LivroRepository

---
+++ src/test/java/com/alura/literalura/repository/AutorRepositoryTests.java
@@ -0,0 +1,27 @@
+package com.alura.literalura.repository;
+
+import com.alura.literalura.model.Autor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+public class AutorRepositoryTests {
+
+    @Autowired
+    private AutorRepository autorRepository;
+
+    @Test
+    public void testSaveAndFindAutor() {
+        Autor autor = new Autor();
+        autor.setNome("Teste Autor");
+        autorRepository.save(autor);
+
+        Optional<Autor> found = autorRepository.findById(autor.getId());
+        assertTrue(found.isPresent());
+    }
+}
