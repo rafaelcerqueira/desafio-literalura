90cb0b0ba9ab339f59956128c9b529fabcaffe72
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2025-01-19 12:13:11 -0300

    feat: interface AutorRepository adicionada ao pacote repository

--- src/main/java/com/alura/literalura/service/LivroService.java
+++ src/main/java/com/alura/literalura/service/LivroService.java
@@ -2,6 +2,7 @@ package com.alura.literalura.service;
 
 import com.alura.literalura.model.Autor;
 import com.alura.literalura.model.Livro;
+import com.alura.literalura.repository.AutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
